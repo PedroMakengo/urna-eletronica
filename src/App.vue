37196f5593c82e3ac58dcea68f84df9d7ec3757c
PedroMakengo <pedromakengomiguel@gmail.com>
2022-09-16 02:43:11 -0300

    Criando a estrutura da minha urna

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Hello World</h1>
+  <div id="app">
+    <Urna />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Urna from "@/components/Urna.vue";
 
-export default defineComponent({});
+export default defineComponent({
+  components: { Urna },
+});
 </script>
+
+<style scope>
+#app {
+  background-color: var(--background-color);
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/Urna.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="urna"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+.urna {
+  width: 75%;
+  height: 70vh;
+
+  background-color: var(--ballot-box-background-color);
+  padding: 30px;
+
+  border-radius: 5px;
+
+  display: flex;
+}
+</style>
