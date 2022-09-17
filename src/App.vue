18289fc4ae2bf3113f1f3839415fcbbab2de414a
PedroMakengo <pedromakengomiguel@gmail.com>
2022-09-17 04:55:49 -0300

    Trabalhando

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Urna from "@/components/Urna.vue";
+import Urna from "@/components/organisms/Urna.vue";
 
 export default defineComponent({
   components: { Urna },
--- src/components/organisms/Urna.vue
+++ src/components/organisms/Urna.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Teclado from "@/components/Teclado.vue";
+import Teclado from "@/components/molecules/Teclado.vue";
 
 export default defineComponent({
   name: "Urna",
